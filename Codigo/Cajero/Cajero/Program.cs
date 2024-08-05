328be898ac7dc9c6f096706e04bc1ad77878ff2f
APRENDIZ <APRENDIZ@BOGDAFCDMFSP071>
2024-08-05 09:42:54 -0500

    consulta

--- Codigo/Cajero/Cajero/Program.cs
+++ Codigo/Cajero/Cajero/Program.cs
@@ -308,6 +308,27 @@ namespace Cajero
                 }
             }
 
+            public void consulta(Cuentas cuenta)
+            {
+                DateTime fecha = DateTime.Now;
+                using (StreamWriter fact = new StreamWriter("C:\\Users\\Fasire\\Desktop\\Factura.txt", true))
+                {
+                    fact.WriteLine("----------------------------------------------");
+                    fact.WriteLine("|Numero de cuenta: {0}", cuenta.saldo);
+                    fact.WriteLine("----------------------------------------------");
+                    fact.WriteLine("CONSULTA DE SALDO");
+                    fact.WriteLine("----------------------------------------------");
+                    fact.WriteLine("|Fecha y hora: {0}", fecha);
+                    fact.WriteLine("----------------------------------------------");
+                    fact.WriteLine("|Total saldo: ${0}", cuenta.saldo);
+                    fact.WriteLine("----------------------------------------------");
+                    fact.WriteLine("|¡Gracias por utilizar nuestros servicios!");
+                    fact.WriteLine("----------------------------------------------");
+                    fact.WriteLine("");
+                    fact.WriteLine("");
+                }
+            }
+
         }
     }
 }
